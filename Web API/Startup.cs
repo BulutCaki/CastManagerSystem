72a3aa8ed9751993fbdf937ae91a934832070db1
BulutCaki <bulutcaki023@gmail.com>
2021-03-13 21:26:12 +0300

    CastManagerSystem

--- Web API/Startup.cs
+++ Web API/Startup.cs
@@ -1,3 +1,5 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -42,7 +44,9 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[] {
+                new CoreModule()
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +60,7 @@ namespace WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
 
             app.UseAuthentication();
 
